c1ae286d30661b2c21da2f0a157b2bf5b5b618ea
Leo23 Chen <Leo23.Chen@advantech.com>
2024-02-08 23:05:35 +0800

    test

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import time, pytz, schedule
 from datetime import datetime
 import datetime as t
 from notices import *
+from streamlit_autorefresh import st_autorefresh
 
-
+st_autorefresh(interval=5000)
 st.title("事項排程通知 !!!")
 taiwan_tz = pytz.timezone("Asia/Taipei") #時區
 st.write(f":green[{t.datetime.now(taiwan_tz)}]")
@@ -15,6 +16,7 @@ st.divider()
 
 schedule.every().wednesday.at("00:00").do(notice_cellphone_tax) # 每月第一個星期三 通知繳電話費
 schedule.every().day.at("23:00").do(notice_sleep)  # 每天 23:00 通知就寢
+schedule.every().day.at("23:10").do(notice_sleep)  # 每天 23:00 通知就寢
 
 
 
--- notices.py
+++ notices.py
@@ -22,7 +22,10 @@ def send_line_notify(notification_message, stickerPackageId, stickerId):
 
 
 
-
+def test():
+    message = f"test"
+    print(message)
+    send_line_notify(message, '6359', '11069871')
 
 
 # 通知就寢
