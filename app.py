2e24321836d3eab150260281b315093e532c4b14
Leo23 Chen <Leo23.Chen@advantech.com>
2024-02-09 00:52:13 +0800

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from notices import *
 from streamlit_autorefresh import st_autorefresh
 import requests
 
-st_autorefresh(interval=5000)
+st_autorefresh(interval=1000)
 st.title("事項排程通知 !!!")
 taiwan_tz = pytz.timezone("Asia/Taipei") #時區
 st.write(f":green[{t.datetime.now(taiwan_tz)}]")
@@ -29,15 +29,18 @@ def test():
     send_line_notify(message, '6359', '11069871')
 
 
-schedule.every().second.do(send_heartbeat)  # 每秒發送心跳
+# schedule.every().second.do(send_heartbeat)  # 每秒發送心跳
 schedule.every().wednesday.at("00:00").do(notice_cellphone_tax) # 每月第一個星期三 通知繳電話費
 schedule.every().day.at("23:00").do(notice_sleep)  # 每天 23:00 通知就寢
 
 schedule.every().minute.at(":00").do(test) 
-schedule.every().minute.at(":00").do(notice_sleep) 
+schedule.every().minute.at(":00").do(send_heartbeat) 
 
 schedule.every().minute.at(":30").do(test) 
-schedule.every().minute.at(":30").do(notice_cellphone_tax) 
+schedule.every().minute.at(":30").do(send_heartbeat) 
+
+schedule.every().hour.at(":00").do(test)
+schedule.every().hour.at(":00").do(send_heartbeat)
 
 while True:
     schedule.run_pending()
